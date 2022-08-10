43f90fb77cf4761650f38ecb6bc14e6093d5950a
Natasha Beddow <beddow90@gmail.com>
2022-08-10 14:25:28 +0100

    added category update

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -27,7 +27,6 @@ router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    // return all categories by quering the db with sequelize
     const oneCategory = await Category.findOne({
       where: {
         id: req.params.id,
@@ -49,7 +48,6 @@ router.get("/:id", async (req, res) => {
 router.post("/", async (req, res) => {
   // create a new category
   try {
-    // return all categories by quering the db with sequelize
     const newCategory = await Category.create({
       category_name: req.body.category_name,
     });
@@ -62,8 +60,21 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!updateCategory) {
+      res.status(404).json({ message: "Failed to update category" });
+    }
+    res.status(200).json(updateCategory);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.delete("/:id", (req, res) => {
