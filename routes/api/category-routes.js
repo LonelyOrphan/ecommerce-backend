f44eafa6c7da94b6a1720de88ca42f93532718b2
Natasha Beddow <beddow90@gmail.com>
2022-08-10 13:40:41 +0100

    added get ID and post routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,7 +8,12 @@ router.get("/", async (req, res) => {
   // be sure to include its associated Products
   try {
     // return all categories by quering the db with sequelize
-    const categoryData = await Category.findAll();
+    const categoryData = await Category.findAll({
+      include: {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    });
     if (!categoryData) {
       res.status(404).json({ message: "Category does not exist" });
     }
@@ -18,13 +23,43 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    // return all categories by quering the db with sequelize
+    const oneCategory = await Category.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    });
+    if (!oneCategory) {
+      res.status(404).json({ message: "Category does not exist" });
+    }
+    res.status(200).json(oneCategory);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
+  try {
+    // return all categories by quering the db with sequelize
+    const newCategory = await Category.create({
+      category_name: req.body.category_name,
+    });
+    if (!newCategory) {
+      res.status(404).json({ message: "Failed to create category" });
+    }
+    res.status(200).json(newCategory);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.put("/:id", (req, res) => {
